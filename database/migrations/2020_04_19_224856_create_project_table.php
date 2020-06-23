874c771ca5319701318bc2a41ca7bbf126ad6b12
tftr <tolgafiratoglu@gmail.com>
2020-06-24 00:38:52 +0300

    project model

--- database/migrations/2020_04_19_224856_create_project_table.php
+++ database/migrations/2020_04_19_224856_create_project_table.php
@@ -17,17 +17,33 @@ class CreateProjectTable extends Migration
             Schema::create('project', function (Blueprint $table) {
                 $table->bigIncrements('id');
                 $table->timestamps();
+                $table->unsignedBigInteger('location');
+                    $table->foreign('location')->references('id')->on('location');
+                $table->unsignedBigInteger('created_by');
+                    $table->foreign('created_by')->references('id')->on('users');
+                $table->unsignedBigInteger('featured_image')->nullable();
+                    $table->foreign('featured_image')->references('id')->on('media');    
+
+                $table->date('estimated_completion_date');
+                $table->boolean('on_sale');
+                $table->date('estimated_date_for_sale')->nullable();
+
+                $table->integer('number_of_properties')->default(1);
+
+                $table->integer('min_number_of_rooms')->default(1);
+                $table->integer('max_number_of_rooms')->default(1);
+
+                $table->decimal('min_price')->default(0);
+                $table->decimal('max_price')->default(1);
+                
+                $table->decimal('lat', 18, 15)->nullable()->default(null);
+                $table->decimal('lon', 18, 15)->nullable()->default(null);
+                
+                $table->boolean('is_approved')->default(false);
+                $table->boolean('is_drafted')->default(false);
+                $table->boolean('is_deleted')->default(false);
+                
             });
         }
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('project');
     }
 }
---
+++ database/migrations/2020_06_23_201600_create_blog_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+}
