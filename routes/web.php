96fe733e223edbc44234c15161bf2f6a9c4eeba9
tftr <tolgafiratoglu@gmail.com>
2020-08-25 00:41:11 +0300

    Before migrating to L7

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/search', 'SearchController@index')->name('search');
 
